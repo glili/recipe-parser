22ef957fa206e8121005aee5e60cc9002312e12b
Your Name <you@example.com>
2021-09-05 23:28:29 -0400

    remove numbers from names

--- src/convert.ts
+++ src/convert.ts
@@ -94,7 +94,7 @@ export function findQuantityAndConvertIfUnicode(ingredientLine: string, language
 
     // If there's a match for the unicodePart in our dictionary above
     if (unicodeObj[unicodePart]) {
-      return [`${numericPart} ${unicodeObj[unicodePart]}`, ingredientLine.replace(getFirstMatch(ingredientLine, unicodeFractionRegex), '').trim()];
+      return [`${numericPart} ${unicodeObj[unicodePart]}`, ingredientLine.replace(getFirstMatch(ingredientLine, unicodeFractionRegex), '').replace(numericPart,'').trim()];
     }
   }
 
